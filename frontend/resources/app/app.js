26d02babb8017f476325030fb15723f2dc2cdc1c
Bradon Hudson <bhud07@gmail.com>
2016-01-03 07:09:27 -0500

    Search Persistence
    Adds search persistence, allowing page to be reloaded and return to
    search results for last query.

--- frontend/resources/app/app.js
+++ frontend/resources/app/app.js
@@ -4,10 +4,47 @@ function switchView(){
     
 }
 function returnNormalView(){
-    $('.resultsContainer').hide();
-    $('.searchContainer').show();
+    
+    window.location.href = "/";
     
 }
+
+
+function setGetParameter(paramName, paramValue)
+{
+    var url = window.location.href;
+    if (url.indexOf(paramName + "=") >= 0)
+    {
+        var prefix = url.substring(0, url.indexOf(paramName));
+        var suffix = url.substring(url.indexOf(paramName));
+        suffix = suffix.substring(suffix.indexOf("=") + 1);
+        suffix = (suffix.indexOf("&") >= 0) ? suffix.substring(suffix.indexOf("&")) : "";
+        url = prefix + paramName + "=" + paramValue + suffix;
+    }
+    else
+    {
+    if (url.indexOf("?") < 0)
+        url += "?" + paramName + "=" + paramValue;
+    else
+        url += "&" + paramName + "=" + paramValue;
+    }
+    window.location.href = url;
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
 
 
 //KEEP WORKING ON THIS!!
@@ -15,6 +52,7 @@ function returnNormalView(){
 var app = angular.module('CSHMovies', []);
 
 app.controller('searchController', function($scope,$http,$sce) {
+    
     $scope.returnNormalView = function(){
         $scope.userQuery = "";
         returnNormalView();
@@ -31,18 +69,7 @@ app.controller('searchController', function($scope,$http,$sce) {
         var query = $scope.userQuery;
         $scope.lastSearch = $scope.userQuery;
         console.log(query); //debug
-        $http.get('resources/api/api.php?query='+query)
-        .success(function(data, status, headers, config) {
-            switchView();
-            console.log(data) //debug
-            $scope.data = data;
-            
-        })
-        .error(function(data, status, headers, config) {             console.log(data); //debug
-            //alert("ERROR: "+status); //debug
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
+        setGetParameter("q",query);
         
     }
     
@@ -58,5 +85,32 @@ app.controller('searchController', function($scope,$http,$sce) {
         document.location.href = newUrl;
         
     }
+    $scope.init = function () {
+        if(window.location.search.indexOf('q=') > -1 && getUrlParameter('q') != "" && getUrlParameter('q') != null){
+            $('.resultsContainer').show();
+            var query = getUrlParameter('q');
+            $scope.lastSearch = query;
+            console.log(query); //debug
+            $scope.userQuery = query;
+            $http.get('resources/api/api.php?query='+query)
+        .success(function(data, status, headers, config) {   
+            switchView();
+            console.log(data) //debug
+            $scope.data = data;
+            
+        })
+        .error(function(data, status, headers, config) {             console.log(data); //debug
+            //alert("ERROR: "+status); //debug
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+        }
+        else{
+            $('.searchContainer').show();
+        }
+        
+        
+    };
+    $scope.init();
     
 });
