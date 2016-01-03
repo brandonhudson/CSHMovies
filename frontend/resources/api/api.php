e7325885691a2029047c07d4130735894bea4bae
Steven Mirabito <steven@stevenmirabito.com>
2016-01-03 06:35:22 -0500

    Fixed some formatting and removed extra whitespace. Added keys.sample.php.

--- frontend/resources/api/api.php
+++ frontend/resources/api/api.php
@@ -1,92 +1,78 @@
 <?php
 
 require 'keys.php';
-
-
 include 'tmdb-api.php';
 
-$apikey = $tmdbKey;
-$tmdb = new TMDB($apikey); // by simply giving $apikey it sets the default lang to 'en'
-
+$tmdb = new TMDB($tmdbKey);
 
 $db = new mysqli($host, $dbusername, $dbpassword, $dbname) or die("Connection Error: " . mysqli_error($db));
 
-if(isset($_GET['query'])){
+if (isset($_GET['query'])) {
     $search = $db->real_escape_string($_GET['query']);
-}else{
+} else {
     http_response_code(501);
-    die('error - q not set');
+    die('error - no query');
 }
 
 $query = "SELECT * FROM movieList WHERE MATCH (title) AGAINST ('$search') LIMIT 1000";
 
-    
+if (!$result = $db->query($query)) {
+    die(http_response_code(400));
+}
 
-if(!$result = $db->query($query)){
-        die(http_response_code(400));
-    }
 $imageBaseURL = 'http://image.tmdb.org/t/p/w185';
 
 $arr = array();
-if($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
         $tempRow = $row;
         $title = $tempRow['title'];
         $type = $tempRow['type'];
         $counter = 0;
         $inArray = false;
+
         // Check to see if already in array
         for ($i = 0; $i < count($arr); ++$i) {
-            if($arr[$i]['title'] == $title){
-                $arr[$i]['server'] = $arr[$i]['server'].", ".$tempRow['server'];
+            if ($arr[$i]['title'] == $title) {
+                $arr[$i]['server'] = $arr[$i]['server'] . ", " . $tempRow['server'];
                 $inArray = true;
                 break;
             }
         }
 
-        
-        if(!$inArray){
-        
-        
-            if($type == "<class 'plexapi.video.Show'>"){
+        if (!$inArray) {
+            if ($type == "show") {
                 $tvShows = $tmdb->searchTVShow($title);
-                foreach($tvShows as $tvShow){
-                    if($counter == 1){
+                foreach ($tvShows as $tvShow) {
+                    if ($counter == 1) {
                         break;
                     }
                     $art = $tvShow->getPoster();
                     $counter++;
                 }
-
-            }
-            else{
+            } else {
                 $movies = $tmdb->searchMovie($title);
-                // returns an array of Movie Object
-
-                foreach($movies as $movie){
-                    if($counter == 1){
+                // Returns an array of Movie objects
+                foreach ($movies as $movie) {
+                    if ($counter == 1) {
                         break;
                     }
-                    $art = $movie->getPoster( );
+                    $art = $movie->getPoster();
                     $counter++;
-                }     
+                }
             }
 
-
-            if($art != NULL){
-                $tempRow['art'] = $imageBaseURL.$art;
+            if ($art != NULL) {
+                $tempRow['art'] = $imageBaseURL . $art;
 
             }
-            $arr[] = $tempRow;	
-	   }
+            $arr[] = $tempRow;
+        }
     }
 }
-# JSON-encode the response
+
+// JSON-encode the response
 $json_response = json_encode($arr);
 
-// # Return the response
+// Return the response
 echo $json_response;
-
-
-
-?>
---
+++ frontend/resources/api/keys.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+// TheMovieDB API Configuration
+$tmdbKey = 'YOUR_TMDB_API_KEY';
+
+// MySQL Database Configuration
+$host = 'localhost';
+$dbusername = 'YOUR_DB_USER';
+$dbpassword = 'YOUR_DB_PASS';
+$dbname = 'cshmovies';
